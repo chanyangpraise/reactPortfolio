0952937c68bba2ab13bcb99b842a02f46c03ad26
Chanyang Choi <cychoi710@icloud.com>
2022-11-14 13:45:53 +0900

    11-14-2022 login page 수정

--- loginpage/routes/index.js
+++ loginpage/routes/index.js
@@ -9,7 +9,7 @@ router.get("/", function (req, res, next) {
 router.post("/login", (req, res) => {
   const id = "testid";
   const pw = "testpw";
-  console.log(req.body); // POST로 보낸 데이터는 해당 객체에 저장 됩니다.
+  console.log(req); // POST로 보낸 데이터는 해당 객체에 저장 됩니다.
   if (id === req.body.id && pw === req.body.pw) {
     res.end("success");
   } else {
