97ff70ff0c14afb64d12d63851274bf0d5dad651
Chanyang Choi <cychoi710@icloud.com>
2022-12-07 17:02:23 +0900

    december file

--- javascript/december/12-07.js
+++ javascript/december/12-07.js
@@ -95,4 +95,13 @@
 
 // ReactDOM.render(<App />, root);
 
-// 5 
+// 6
+function solution(n) {
+  var answer = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      answer = answer + 1;
+    }
+  }
+  return answer;
+}
