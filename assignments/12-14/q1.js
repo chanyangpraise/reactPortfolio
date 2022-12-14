1e687edf2ab38467484b30657230dfa6b43bb96c
Chanyang Choi <cychoi710@icloud.com>
2022-12-14 17:08:06 +0900

    12-14 teacher's solutions

--- assignments/12-14/q1.js
+++ assignments/12-14/q1.js
@@ -1,3 +1,4 @@
+// solved with teacher's help 
 function solution(numbers, direction) {
 	var answer = [];
 	if (direction === "right") {
@@ -13,3 +14,41 @@ function solution(numbers, direction) {
 }
 
 console.log(solution(([1, 2, 3, 4, 5, 6, 7]), "left"));
+
+// 1번문제 for문
+// right -> 배열의 끝에 있는 것이 배열의 첫번째로 간다.
+//            pop -> unshift를 하면 된다.
+
+// left -> 배열의 첫번째 있는 것이 배열의 마지막으로 간다
+//           shift -> push
+function solution(numbers, direction) {
+  var answer = [];
+  // numbers = [1,2,3]
+  if (direction === "right") {
+    let pop = numbers.pop(); // 3
+    numbers.unshift(pop); // [3, 1, 2]
+    answer = nunbers;
+    // answer = numbers.unshift(numbers.pop())
+  } else {
+    let shift = numbers.shift(); // 1
+    numbers.push(shift);
+    answer = numbers;
+  }
+  return answer;
+}
+
+// 1번문제 구조분해 할당
+function solution(numbers, direction) {
+  var answer = [];
+  if (direction === "right") {
+    // answer = [numbers.pop(), ...numbers];
+    answer = [
+      numbers[numbers.length - 1],
+      ...numbers.slice(0, numbers.length - 1),
+    ];
+  } else {
+    slice ->
+    answer = [...numbers.slice(1, numbers.length), numbers[0]];
+  }
+  return answer;
+}
--- assignments/12-14/q2.js
+++ assignments/12-14/q2.js
@@ -1,3 +1,4 @@
+// solved with teacher's help
 function solution(num, k) {
   let answer = 0;
   let text = num.toString();
@@ -8,5 +9,33 @@ function solution(num, k) {
   } else {
     answer = b + 1;
   }
+  return answer;
+}
+
+// teacher solution
+function solution(num, k) {
+  var answer = 0;
+  // num은 정수 string이나 array로 변환이 필요하다.
+  let a = num.toString(); // 정수 -> 문자열
+  let b = a.indexOf(k); // k의 index를 찾음
+  // 없으면 -1, 있으면 0부터 시작하는 index를 반환
+  if (b !== -1) {
+    answer = b + 1;
+  } else {
+    answer = -1;
+  }
+  return answer;
+}
+
+function solution(num, k) {
+  return (" " + num).indexOf(k);
+}
+
+function solution(num, k) {
+  let answer = 0;
+  let arrayNum = num.toString().split("");
+  let addArrayNum = [" ", ...arrayNum];
+  answer = addArrayNum.indexOf(k);
+  // index가 0부터 시작합니다.
   return answer;
 }
